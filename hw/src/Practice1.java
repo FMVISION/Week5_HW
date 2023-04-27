d701fae70817fe07f6741e7de17f922d67735777
FMVISION <fmvision21@gmail.com>
2023-04-27 23:00:53 +0100

    done

---
+++ hw/src/Practice1.java
@@ -0,0 +1,14 @@
+public class Practice1 {
+
+    public static void main(String[] args) {
+        //  datatype
+
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+
+
+        System.out.println("c");
+
+    }
+}
